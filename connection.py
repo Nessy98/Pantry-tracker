44a102fbf23166a4e36813e7305fa64b9d3c565e
VanessaStoynova <vanessa.stoynova98@gmail.com>
2020-06-11 23:42:35 +0300

    Removed unneeded *.pyc files
    Signed-off-by: VanessaStoynova <vanessa.stoynova98@gmail.com>

--- connection.py
+++ connection.py
@@ -4,7 +4,7 @@ connection = sqlite3.connect('pantry.db')
 
 # Create table
 #connection.execute('''CREATE TABLE IF NOT EXISTS foods
-#             (barcode test, name text )''')
+#             (barcode text, name text )''')
 
 #c.execute('''INSERT INTO foods
 #             VALUES(101010, 'coca-cola')''')
